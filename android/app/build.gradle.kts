7424243e37de3de56c23c20d231e0176bbb43c7f
raihan sikdar <raihansikdar10@gmail.com>
2025-06-16 07:29:03 +0600

    modify

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
+            keyAlias = keystoreProperties.getProperty("keyAlias") ?: ""
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String
